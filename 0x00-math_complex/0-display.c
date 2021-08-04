03aa653eadc2bcc1341b4abfd9ace1979b828e69
luffy981 <jhonsmith981@hotmail.com>
2021-08-04 02:18:16 +0000

    Function to display a complex number

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,9 +6,21 @@
 void display_complex_number(complex c)
 {
 	if (c.im != 0 && c.re != 0)
-		printf("%.0f + %.0fi\n", c.re, c.im);
+	{
+		if (c.im == 1)
+			printf("%.0f + i\n", c.re);
+		else
+			printf("%.0f + %.0fi\n", c.re, c.im);
+	}
 	else if (c.re != 0 && c.im == 0)
 		printf("%.0f\n", c.re);
 	else if (c.re == 0 && c.im != 0)
-		printf("%.0fi\n", c.im);
+	{
+		if (c.im == 1)
+			printf("i\n");
+		else
+			printf("%.0fi\n", c.im);
+	}
+	else if (c.re == 0 && c.im == 0)
+		printf("%d\n", 0);
 }
