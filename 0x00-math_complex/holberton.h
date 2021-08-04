2b06ec3a27753c5dc322a592f19bedc7998b25a7
luffy981 <jhonsmith981@hotmail.com>
2021-08-04 01:53:52 +0000

    File headers

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -1,5 +1,17 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+/**
+ * struct complex - Struct to complex number
+ * @re: Real number
+ * @im: Imaginary number
+ *
+ * Description: Struct  to complex number
+ */
+typedef struct complex
+{
+	double re;
+	double im;
+} complex;
 void display_complex_number(complex c);
 complex conjugate(complex c);
 double modulus(complex c);
@@ -8,5 +20,5 @@ void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
 void division(complex c1, complex c2, complex *c3);
-void complex_from_mod_arg (double m, double arg, complex *c3);
+void complex_from_mod_arg(double m, double arg, complex *c3);
 #endif
