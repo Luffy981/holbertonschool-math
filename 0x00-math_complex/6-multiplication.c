d376478a9618e9f415075a0fdc6569cc56bd7a94
luffy981 <jhonsmith981@hotmail.com>
2021-08-11 05:44:05 +0000

    function that performs the multiplication operation to complex numbers

---
+++ 0x00-math_complex/6-multiplication.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * multiplication - function  multiplication operation to complex numbers
+ * @c1: Struct complex
+ * @c2: Struct complex
+ * @c3: Struct complex
+ */
+void multiplication(complex c1, complex c2, complex *c3)
+{
+	(*c3).re = ((c1.re * c2.re) - (c1.im * c2.im));
+	(*c3).im = ((c1.re * c2.im) + (c1.im * c2.re));
+}
