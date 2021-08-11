239ecd6708c9dbf40d4ffbb4511208128ac56a0c
luffy981 <jhonsmith981@hotmail.com>
2021-08-11 05:54:26 +0000

    function that performs the division operation to complex numbers

---
+++ 0x00-math_complex/7-division.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * division - function that performs the division operation to complex numbers
+ * @c1: Struct complex
+ * @c2: Struct complex
+ * @c3: Struct complex
+ */
+void division(complex c1, complex c2, complex *c3)
+{
+	double = a;
+	a = (c2.re * c2.re + c2.im * c2.im);
+	(*c3).re = ((c1.re * c2.re) + (c1.im * c2.im)) / a;
+	(*c3).im = ((c1.im * c2.re) - (c1.re * c2.im)) / a;
+}
