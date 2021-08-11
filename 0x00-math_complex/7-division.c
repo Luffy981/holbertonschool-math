6f7385838445be97a6c98ff545dfbf130ed349d1
luffy981 <jhonsmith981@hotmail.com>
2021-08-11 05:57:02 +0000

    function that performs the division operation to complex numbers

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -7,7 +7,8 @@
  */
 void division(complex c1, complex c2, complex *c3)
 {
-	double = a;
+	double a;
+
 	a = (c2.re * c2.re + c2.im * c2.im);
 	(*c3).re = ((c1.re * c2.re) + (c1.im * c2.im)) / a;
 	(*c3).im = ((c1.im * c2.re) - (c1.re * c2.im)) / a;
