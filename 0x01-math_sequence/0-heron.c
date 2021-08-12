c460c5c112ccdbf5596bdff1be89e68b6f78028d
luffy981 <jhonsmith981@hotmail.com>
2021-08-12 21:48:47 +0000

    Heron

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
-t_cell *heron(double p, double x0);
+#include "heron.h"
+t_cell *heron(double p, double x0)
 {
 	t_cell *node;
-	t_cell *head;
 	node = malloc(sizeof(t_cell));
 	node->elt = x0;
-	while(x0 >= 1/10000000)
+	if(p - x0 >= 1/10000000)
 	{
 		x0 = (x0 + p/x0)/2;
-		head = heron(p, x0);
+		node->next = heron(p, x0);
 	}
-	if(x0 < 1/10000000)
+	else
 	{
 		return(node->next = NULL);
 	}
+	return(node);
 }
