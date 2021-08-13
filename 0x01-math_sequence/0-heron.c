e38d026136dc38aa835e3b5a495fee0e9ee7f13e
luffy981 <jhonsmith981@hotmail.com>
2021-08-13 00:04:06 +0000

    function that return the Heron sequence until having convergence

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -1,17 +1,33 @@
 #include "heron.h"
+/**
+ * heron - function that return the Heron sequence until having convergence
+ * @p: Number
+ * @x0: Number
+ *
+ * Return: Return count
+ */
 t_cell *heron(double p, double x0)
 {
 	t_cell *node;
+	t_cell *head;
+	t_cell *ptr;
+
 	node = malloc(sizeof(t_cell));
 	node->elt = x0;
-	if(p - x0 >= 1/10000000)
+	node->next = NULL;
+	if (fabs(p - (x0 * x0)) > (1 / 10000000))
 	{
-		x0 = (x0 + p/x0)/2;
-		node->next = heron(p, x0);
-	}
-	else
+		x0 = (x0 + (p / x0)) / 2;
+		head = heron(p, x0);
+	} else
 	{
-		return(node->next = NULL);
+		return (node);
 	}
-	return(node);
+	ptr = head;
+	while (head->next != NULL)
+	{
+		head = head->next;
+	}
+	head->next = node;
+	return (ptr);
 }
