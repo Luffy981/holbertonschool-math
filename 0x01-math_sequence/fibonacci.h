739cab21f7ee8ada04fce0df30f2c04b141853f8
luffy981 <jhonsmith981@hotmail.com>
2021-08-13 00:10:22 +0000

    Header file fibonacci

---
+++ 0x01-math_sequence/fibonacci.h
@@ -0,0 +1,21 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct sequence - Struct to sequence
+ * @elt: Real number
+ * @next: Imaginary number
+ *
+ * Description: Struct  to complex number
+ */
+typedef struct sequence
+{
+	double elt;
+	struct sequence *next;
+} t_cell;
+
+t_cell *Fibonnaci();
+double gold_number(t_cell *head);
+#endif
